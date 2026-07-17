7244d01fcae60eb2e410e9c267cbd6aaad109961
Riccardo Santini <190469536+ricksan37@users.noreply.github.com>
2026-07-17 19:40:43 +0200

    Phase 2: stg_ft_offres complet - extraction 14 champs, dedup sur offre_id (553 lignes)

--- observatoire/models/staging/stg_ft_offres.sql
+++ observatoire/models/staging/stg_ft_offres.sql
@@ -1,1 +1,18 @@
-select * from {{ source('raw', 'ft_offres') }}
+select
+    offre.id as offre_id,
+    offre.intitule as intitule,
+    offre.dateCreation::timestamp as date_creation,
+    offre.dateActualisation::timestamp as date_actualisation,
+    offre.romeCode as rome_code,
+    offre.romeLibelle as rome_libelle,
+    offre.typeContrat as type_contrat,
+    offre.experienceExige as experience_exige,
+    offre.lieuTravail.codePostal as code_postal,
+    offre.lieuTravail.commune as commune,
+    offre.entreprise.nom as entreprise_nom,
+    offre.codeNaf as code_naf,
+    offre.salaire.libelle as salaire_libelle,
+    offre.nombrePostes as nombre_postes
+from {{ source('raw', 'ft_offres') }},
+     unnest(resultats) as t(offre)
+     qualify row_number() over (partition by offre_id order by date_actualisation desc) = 1
